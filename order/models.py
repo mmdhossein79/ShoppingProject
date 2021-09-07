1d9cd0bb30de10ae12a1c4790b98b3d1b917cb86
mmdhossien <mohammad49517879@gmail.com>
2021-09-07 12:21:27 +0430

    edit_models

--- order/models.py
+++ order/models.py
@@ -39,27 +39,6 @@ class OrderItem(models.Model):
         return self.price * self.quantity
 
 
-# class UniqueCodes(models.Model):
-#     code = models.CharField(max_length=8, blank=True, null=True, unique=True)
-
-    # @classmethod
-    # def post_create(cls, sender, instance, created, *args, **kwargs):
-    #     # If new database record
-    #     if created:
-    #         # We have the primary key (ID Field) now so let's grab it
-    #         id_string = str(instance.id)
-    #         # Define our random string alphabet (notice I've omitted I,O,etc. as they can be confused for other characters)
-    #         upper_alpha = "ABCDEFGHJKLMNPQRSTVWXYZ"
-    #         # Create an 8 char random string from our alphabet
-    #         random_str = "".join(secrets.choice(upper_alpha) for i in range(8))
-    #         # Append the ID to the end of the random string
-    #         instance.code = (random_str + id_string)[-8:]
-    #         # Save the class instance
-    #         instance.save()
-    #
-    # def __str__(self):
-    #     return "%s" % (self.code,)
-
 
 class code(models.Model):
     @staticmethod
--- product/models.py
+++ product/models.py
@@ -4,7 +4,9 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField(max_length=150, db_index=True)
-    parent = models.ForeignKey('Category',on_delete=models.CASCADE)
+ #   parent = models.ForeignKey('Category',on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.name
